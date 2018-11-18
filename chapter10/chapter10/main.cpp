0a542fd8fd2f81747a2337779301a55b3675496b
杲申申 <zhuangziyanjiuhui@163.com>
2018-11-18 22:44:29 +0800

    to 10-9

--- chapter10/chapter10/main.cpp
+++ chapter10/chapter10/main.cpp
@@ -12,7 +12,7 @@
 #include <functional>
 #include <algorithm>
 using namespace std;
-void ch11_3()
+void ch10_3()
 {
     vector<int> ivector;
     cout<<ivector.capacity()<<endl;
@@ -34,7 +34,7 @@ struct ge{
 };
 
 template<typename container>
-void ch11_5(int n,int m)
+void ch10_5(int n,int m)
 {
     container c;
    c.resize(n);
@@ -75,7 +75,7 @@ void ch11_5(int n,int m)
 }
 
 template<typename container>
-void ch11_5noerase(int n,int m)
+void ch10_5noerase(int n,int m)
 {
     container c;
     c.resize(n);
@@ -107,21 +107,126 @@ void ch11_5noerase(int n,int m)
     c.erase(remove(c.begin(),c.end(),-1),c.end());
     copy(c.begin(), c.end(),ostream_iterator<int>(cout," "));
 }
-int main(int argc, const char * argv[]) {
+void ch10_5demo()
+{
     int n=2000000,m=10;
     //vector
     time_t begin=time(0);
-    ch11_5noerase<vector<int>>(n,m);
+    ch10_5noerase<vector<int>>(n,m);
     time_t end1=time(0);
     cout<<"vector time: "<< end1-begin<<endl;
     //deque
-    ch11_5noerase<deque<int>>(n,m);
+    ch10_5noerase<deque<int>>(n,m);
     time_t end2=time(0);
     cout<<"deque time: "<< end2-end1<<endl;
     
     //list
-    ch11_5<list<int>>(n,m);
+    ch10_5<list<int>>(n,m);
     time_t end3=time(0);
     cout<<"list time: "<< end3-end2<<endl;
+}
+
+template<class T>
+void exchange(list<T>& l1, typename list<T>::iterator p1,
+              list<T>& l2, typename list<T>::iterator p2)
+{
+    l1.splice(p1,l2,p2,l2.end());
+    l2.splice(l2.end(),l1,p1,l1.end());
+//    l1.insert(p1,p2,l2.end());
+//    l2.insert(p2,p1,l1.end());
+//    l1.erase(p1,l1.end());
+//    l2.erase(p2,l2.end());
+}
+
+void ch10_6demo()
+{
+    list<int> l1({1,2,3,4});
+     list<int> l2({11,12,13,14});
+    exchange(l1,++++l1.begin(),l2,++++l2.begin());
+    copy(l1.begin(),l1.end(),ostream_iterator<int>(cout, " "));
+    cout<<endl;
+    copy(l2.begin(),l2.end(),ostream_iterator<int>(cout, " "));
+    cout<<endl;
+}
+
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+template<class T>
+void ch10_7()
+{
+    T c;
+    cout<<typeid(T).name()<<" ";
+    c.push(5);
+    c.push(1);
+    c.push(4);
+    c.push(6);
+    cout<<c.top()<<" ";
+    c.pop();
+    cout<<c.top()<<" ";
+    c.pop();
+    c.push(2);
+    c.push(3);
+    cout<<c.top()<<" ";
+    c.pop();
+    cout<<c.top()<<" ";
+    c.pop();
+    cout<<endl;
+}
+void ch10_7demo()
+{
+    ch10_7<stack<int>>();
+    //ch10_7<queue<int>>();
+   ch10_7<priority_queue<int,vector<int>,less<int> >>();
+}
+
+void ch10_8demo(){
+    multiset<char> chars;
+    map<char,int> charmap;
+    char c;
+    while(cin.get(c))
+    {
+      
+      
+        size_t count=chars.count(c);
+        if(count==0)
+            cout<<"not found ";
+        else
+            cout<<"count: "<<count<<" ";
+        cout<<endl;
+        chars.insert(c);
+        
+        int d=++charmap[c];
+        if(d==1)
+            cout<<"not found ";
+        else
+            cout<<"count: "<<d-1<<" ";
+        cout<<endl;
+        
+        
+    }
+    
+}
+
+void ch10_9demo()
+{
+    int array1[]={1,2,3,4,5,6,7,8};
+    int array2[]={11,12,13,14,15,16,17,18};
+    //vector<int> v1(array1,array1+sizeof(array1)/sizeof(int));
+    swap(array1[2],array1[4]);
+    iter_swap(array1+2,array1+4);
+    swap_ranges(array1, array1+3, array1+1);
+  //  rotate(array1,array1+3,array1+6);
+    copy(array1,array1+8,ostream_iterator<int>(cout));
+    
+}
+
+int main(int argc, const char * argv[]) {
+    //ch10_5demo();
+    //ch10_6demo();
+   // ch10_7demo();
+    //ch10_8demo();
+      ch10_9demo();
     return 0;
 }
